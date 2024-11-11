48b7af41bb2a12c1ce035b39977b0cf6dcc41cd9
Harsh Patel <harshp5@usf.edu>
2024-11-10 19:25:14 -0500

    Create transaction module with Transaction class.

---
+++ project-folder/transaction.js
@@ -0,0 +1,31 @@
+import { assets, getAssetById } from './asset.js';
+
+// Making the Transaction class
+export class Transaction {
+    constructor(assetId, type, quantity) {
+        this.assetId = assetId;
+        this.type = type;
+        this.quantity = quantity;
+    }
+
+   
+     execute() {
+        const asset = getAssetById(this.assetId); // this is to get the asset by ID
+        if (!asset) {
+            throw new Error("Asset not found");
+        }
+
+        if (this.type === "buy") {
+            asset.quantity += this.quantity;
+            console.log(`${this.quantity} units of ${asset.name} bought.`);
+        } else if (this.type === "sell") {
+            if (asset.quantity < this.quantity) {  // these are the error statments
+                throw new Error(`Insufficient quantity for sale of ${asset.name}`);
+            }
+            asset.quantity -= this.quantity;
+            console.log(`${this.quantity} units of ${asset.name} sold.`);
+        } else {
+            throw new Error("Invalid transaction type ");
+        }
+    }
+}
